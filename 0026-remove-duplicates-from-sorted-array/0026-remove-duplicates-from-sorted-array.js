a3e00b94b00027ec9cca4eaefaa9504f032dcda8
Hwi Yeong Jang <87405971+hwiiy@users.noreply.github.com>
2022-11-23 14:51:26 +0900

    Time: 221 ms (11.32%), Space: 48.4 MB (5.11%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+var removeDuplicates = function(nums) {
+  for(let i=0;i<nums.length-1;){
+    if(nums[i]===nums[i+1]){
+      nums.splice(i,1)
+    }else {
+      i++
+    }
+  }
+ console.log(nums)
+}
